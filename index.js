3e111b258f3f7d6de323f3ace58e280118bc7b7b
PranaliAdsul <98943009+PranaliAdsul@users.noreply.github.com>
2023-05-26 13:29:44 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const container = document.querySelector('.container');
-const slider = document.querySelector('.slider');
-
-slider.addEventListener('input', () => {
-  container.style.setProperty('--new-position', `${slider.value}%`);
- })
